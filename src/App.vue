dc12b1521dd196ac450e2500ecc1a92c0fc70146
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2022-09-28 14:56:23 +0400

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark> </v-app-bar>
+
+    <v-main>
+      <v-btn color="primary">Hello</v-btn>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data: () => ({})
+}
+</script>
